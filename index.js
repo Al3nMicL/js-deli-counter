1e35123833c7ae2c619a649d8e8d2599cc472a65
Al3nMicL <https://twitter.com/Al3nMicL>
2019-02-11 15:36:31 -0500

    nowServing() tests pass

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ let takeANumber = (line, name) => {
 	if (Array.isArray(line)) {
   	line.push(name);	
   };
-  
+
   let position = line.length; 
  
  return `Welcome, ${name}. You are number ${position} in line.`;
+};
+
+// Task 2 - now serving function
+let nowServing = (katzDeliLine) => {
+ if (katzDeliLine.length > 1) {
+ 	let person = katzDeliLine.shift();
+ 	return `Currently serving ${person}.`
+ } else {
+ 	return "There is nobody waiting to be served!";
+ };
+
 };
